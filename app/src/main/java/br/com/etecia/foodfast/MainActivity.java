8ca4e27441984249636c78355394edbf1c7d75cb
lndsilva <lndsilva@hotmail.com>
2022-10-27 21:40:44 -0300

    clicando no menu

--- app/src/main/java/br/com/etecia/foodfast/MainActivity.java
+++ app/src/main/java/br/com/etecia/foodfast/MainActivity.java
@@ -1,8 +1,13 @@
 package br.com.etecia.foodfast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    //inserindo o menu na barra de aplicativos
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+    //Criando o método para clicar nos itens do menu
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.mFav:
+                Toast.makeText()
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
