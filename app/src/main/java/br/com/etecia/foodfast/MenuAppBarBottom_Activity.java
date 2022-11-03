567449735b71db3af00aeae5cb4cbfc8d9a666cf
lndsilva <lndsilva@hotmail.com>
2022-11-03 20:20:18 -0300

    criando a barra de ferramentas appBar top

---
+++ app/src/main/java/br/com/etecia/foodfast/MenuAppBarBottom_Activity.java
@@ -0,0 +1,14 @@
+package br.com.etecia.foodfast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MenuAppBarBottom_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.menu_app_bar_bottom_layout);
+    }
+}
---
+++ app/src/main/java/br/com/etecia/foodfast/MenuAppBarTop_Activity.java
@@ -0,0 +1,15 @@
+package br.com.etecia.foodfast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MenuAppBarTop_Activity extends AppCompatActivity {
+    //Vamos tomar café
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.menu_app_bar_top_layout);
+    }
+}
