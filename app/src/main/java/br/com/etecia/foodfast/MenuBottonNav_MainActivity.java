d3e3cb286f86865c21bfe6a754ea19ec81ed7384
lndsilva <lndsilva@hotmail.com>
2022-11-10 19:48:05 -0300

    bottom nav

---
+++ app/src/main/java/br/com/etecia/foodfast/MenuBottonNav_MainActivity.java
@@ -0,0 +1,14 @@
+package br.com.etecia.foodfast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MenuBottonNav_MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.menu_botton_nav_main_layout);
+    }
+}
