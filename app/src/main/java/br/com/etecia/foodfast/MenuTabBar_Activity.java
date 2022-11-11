c0c0d3a2d15a2aaa9de95f1d8792ee783d7513c8
lndsilva <lndsilva@hotmail.com>
2022-11-10 22:01:35 -0300

    criando tablayout

---
+++ app/src/main/java/br/com/etecia/foodfast/MenuTabBar_Activity.java
@@ -0,0 +1,14 @@
+package br.com.etecia.foodfast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MenuTabBar_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.menu_tab_bar_layout);
+    }
+}
