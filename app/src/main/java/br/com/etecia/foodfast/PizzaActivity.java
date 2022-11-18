7cafedefed0417d3b03302842b6a13c40ab40278
lndsilva <lndsilva@hotmail.com>
2022-11-17 22:11:56 -0300

    criando listView e modelo para carregar na lista

---
+++ app/src/main/java/br/com/etecia/foodfast/PizzaActivity.java
@@ -0,0 +1,14 @@
+package br.com.etecia.foodfast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PizzaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.pizza_layout);
+    }
+}
